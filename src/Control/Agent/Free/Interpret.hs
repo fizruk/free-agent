22cd2386af32d5be9d76850e3fd7dce0528c2616
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2013-09-23 22:07:14 +0000

    interpret agent programs

---
+++ src/Control/Agent/Free/Interpret.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+---------------------------------------------------------------------------
+-- |
+-- Module      :  Control.Agent.Free.Interpret
+-- Copyright   :  (c) Nickolay Kudasov 2013
+-- License     :  BSD-style (see the file LICENSE)
+-- 
+-- Maintainer  :  nickolay.kudasov@gmail.com
+-- Stability   :  experimental
+-- Portability :  non-portable (Rank2Types, MPTCs)
+--
+-- Interpreting free agent programs.
+---------------------------------------------------------------------------
+module Control.Agent.Free.Interpret where
+
+import Control.Monad.Trans.Class
+import Control.Monad.Free
+import Control.Agent.Free
+import Control.Monad (void)
+
+-- | Interpretation of free monadic values.
+class (Functor f, Functor m, Monad m) => MonadInterpret f m where
+    -- | Interpret single command.
+    interpretF :: f (m a) -> m a
+    -- | Interpret complete free monadic value.
+    interpretM :: Free f a -> m a
+    interpretM = iterM interpretF
+
+-- | Like 'interpretM', but throws away the result.
+interpretM_ :: (MonadInterpret f m) => Free f a -> m ()
+interpretM_ = void . interpretM
+
+-- | Execute agent program with particular interpreter.
+execAgent  :: (Monad (t (Free f)), MonadTrans t, MonadInterpret f m) => (forall n b. Monad n => t n b -> n b) -> Agent t f a -> m a
+execAgent run = interpretM . run . runAgent
+
+-- | Like 'execAgent', but throws away the result.
+execAgent_ :: (Monad (t (Free f)), MonadTrans t, MonadInterpret f m) => (forall n b. Monad n => t n b -> n b) -> Agent t f a -> m ()
+execAgent_ run = void . execAgent run
