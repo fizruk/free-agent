429a6e0e6c562e01cb27091c709457a5a4d72df0
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2013-11-24 00:36:19 +0400

    added ABT family module

---
+++ src/Control/Agent/Free/Algorithms/ABT.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE FlexibleContexts #-}
+---------------------------------------------------------------------------
+-- |
+-- Module      :  Control.Agent.Free.Algorithms.ABT
+-- Copyright   :  (c) Nickolay Kudasov 2013
+-- License     :  BSD-style (see the file LICENSE)
+-- 
+-- Maintainer  :  nickolay.kudasov@gmail.com
+-- Stability   :  experimental
+-- Portability :  ghc
+--
+-- Family of ABT algorithms.
+---------------------------------------------------------------------------
+module Control.Agent.Free.Algorithms.ABT where
