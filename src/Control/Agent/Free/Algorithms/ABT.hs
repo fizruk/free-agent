005f2fa12aae135be601df6ff8125b19c5eb670b
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2013-11-24 03:53:27 +0400

    added documentation

--- src/Control/Agent/Free/Algorithms/ABT.hs
+++ src/Control/Agent/Free/Algorithms/ABT.hs
@@ -11,9 +11,25 @@
 -- Stability   :  experimental
 -- Portability :  ghc
 --
--- Family of ABT algorithms.
+-- Family of ABT algorithms. Based on "The Asynchronous Backtracking Family"
+-- <http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.60.4716>.
 ---------------------------------------------------------------------------
-module Control.Agent.Free.Algorithms.ABT where
+module Control.Agent.Free.Algorithms.ABT (
+  -- * Algorithms
+    A
+  , abtKernel
+  -- * ABT Kernel API
+  , ABTKernelF(..)
+  , sendOk
+  , sendBacktrack
+  , sendStop
+  , recv
+  -- * Used data structures
+  , Message(..)
+  , NoGood(..)
+  , AgentState(..)
+  , initialAgentState
+) where
 
 import Control.Arrow
 import Control.Agent.Free
@@ -30,54 +46,77 @@ import qualified Data.Set as Set
 import Data.Map (Map)
 import qualified Data.Map as Map
 
+-- | ABT Kernel messages.
 data Message i v
-  = MsgOk i v
+  = -- | OK? message is sent to higher agents so they could check it.
+    MsgOk i v
+    -- | BACKTRACK message is sent to lower agent to force it rechoose its value.
   | MsgNoGood i (NoGood i v)
+    -- | STOP message is sent when it is know that a problem has no solution.
   | MsgStop
   deriving (Show)
 
+-- | 'NoGood' is a witness of a previous conflict. It is of the form
+-- @x_i1 = v1 && ... && x_iN = vN  => x_j /= v@, where @x_j@ refers to the agent holding
+-- this nogood in its nogood store.
 data NoGood i v = NoGood
-  { ngdLHS :: Map i v
-  , ngdRHS :: (i, v)
+  { ngdLHS :: Map i v   -- ^ Left hand side of a nogood.
+  , ngdRHS :: (i, v)    -- ^ Right hand side of a nogood.
   }
   deriving (Show)
 
+-- | Abstract interface used by ABT Kernel algorithm.
 data ABTKernelF i v next
-  = SendOk i v next
+  = -- | Send OK? message. See also 'sendOk'.
+    SendOk i v next
+    -- | Send BACKTRACK message. See also 'sendBacktrack'.
   | SendBacktrack i (NoGood i v) next
+    -- | Send STOP to the *system*. See also 'sendStop'.
   | SendStop next
+    -- | Receive a message. See also 'recv'.
   | Recv (Message i v -> next)
   deriving (Functor)
 
+-- | Send OK? message. Requires address of another agent and a chosen value.
 sendOk :: MonadFree (ABTKernelF i v) m => i -> v -> m ()
 sendOk i v = liftF $ SendOk i v ()
 
+-- | Send BACKTRACK message. Requires address of another agent and resolved nogood store.
 sendBacktrack :: MonadFree (ABTKernelF i v) m => i -> (NoGood i v) -> m ()
 sendBacktrack i ngd = liftF $ SendBacktrack i ngd ()
 
+-- | Send STOP message to the *system*. All agents in the system will receive this message.
 sendStop :: MonadFree (ABTKernelF i v) m => m ()
 sendStop = liftF $ SendStop ()
 
+-- | Receive message.
 recv :: MonadFree (ABTKernelF i v) m => m (Message i v)
 recv = liftF $ Recv id
 
+-- | Agent view is just a 'Map' from agents' adresses to agents' values.
 type AgentView i v = Map i v
 
+-- | A general constraint. @'Constraint' i v@ is agent-oriented and is just a wrapper for
+-- the function of type @'AgentView' i v -> v -> Maybe ('NoGood' i v)@. The latter checks
+-- whether constraint would hold given agent's view and a value it's going to choose. If constraint
+-- wouldn't hold, the function returns @Just ngd@ (it generates 'NoGood'), otherwise it returns 'Nothing'.
 newtype Constraint i v = Constraint
   { constraintCheck  :: AgentView i v -> v -> Maybe (NoGood i v) }
 
+-- | State of an agent for ABT Kernel algorithm.
 data AgentState i v = AgentState
-  { agStop        :: Bool
-  , agValue       :: Maybe v
-  , agDomain      :: [v]
-  , agId          :: i
-  , agView        :: Map i v
-  , agAbove       :: [i]
-  , agBelow       :: [i]
-  , agConstraints :: [Constraint i v]
-  , agNoGoods     :: [NoGood i v]
+  { agStop        :: Bool             -- ^ Flag informing whether algorithm should stop.
+  , agValue       :: Maybe v          -- ^ Currently chosen value.
+  , agDomain      :: [v]              -- ^ Domain of agent's value.
+  , agId          :: i                -- ^ ID (address) of an agent.
+  , agView        :: AgentView i v    -- ^ Current agent's view.
+  , agAbove       :: [i]              -- ^ IDs of higher agents.
+  , agBelow       :: [i]              -- ^ IDs of lower agents.
+  , agConstraints :: [Constraint i v] -- ^ Constraints involving an agent.
+  , agNoGoods     :: [NoGood i v]     -- ^ Agent's nogood store.
   }
 
+-- | Initial agent's state.
 initialAgentState :: AgentState i v
 initialAgentState = AgentState
   { agStop        = False
@@ -91,11 +130,13 @@ initialAgentState = AgentState
   , agNoGoods     = []
   }
 
+-- | A useful alias for a monad in which ABT algorithms run.
 type A i v a = forall m. Monad m => StateT (AgentState i v) (Agent' (ABTKernelF i v) m) a
 
 updateNoGoods :: MonadState (AgentState i v) m => ([NoGood i v] -> [NoGood i v]) -> m ()
 updateNoGoods f = modify (\s@AgentState{agNoGoods=ngds} -> s{ agNoGoods = f ngds })
 
+-- | ABT Kernel algorithm.
 abtKernel :: (Ord i, Eq v) => A i v (Maybe v)
 abtKernel = do
   checkAgentView
