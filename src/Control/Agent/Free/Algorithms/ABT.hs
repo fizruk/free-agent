91bcc669fb9cfe644608ab2926b5eb61a85a9333
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2013-11-24 00:40:19 +0400

    basic structures for ABT Kernel

--- src/Control/Agent/Free/Algorithms/ABT.hs
+++ src/Control/Agent/Free/Algorithms/ABT.hs
@@ -14,3 +14,52 @@
 -- Family of ABT algorithms.
 ---------------------------------------------------------------------------
 module Control.Agent.Free.Algorithms.ABT where
+
+import Control.Arrow
+import Control.Agent.Free
+import Control.Lens
+import Control.Monad.Trans.Free
+import Control.Monad.Free.TH
+
+import qualified Data.Foldable as F
+
+import Data.Map (Map)
+import qualified Data.Map as Map
+
+data Message i v
+  = MsgOk i v
+  | MsgNoGood i (NoGood i v)
+  | MsgStop
+  deriving (Show)
+
+data NoGood i v = NoGood
+  { ngdLHS :: Map i v
+  , ngdRHS :: (i, v)
+  }
+  deriving (Show)
+
+data ABTKernelF i v next
+  = SendOk i v next
+  | SendBacktrack i (NoGood i v) next
+  | SendStop next
+  | Recv (Message i v -> next)
+  deriving (Functor)
+makeFree ''ABTKernelF
+
+type AgentView i v = Map i v
+
+newtype Constraint i v = Constraint
+  { constraintCheck  :: AgentView i v -> v -> Maybe (NoGood i v) }
+
+data AgentState i v = AgentState
+  { _agStop        :: Bool
+  , _agValue       :: Maybe v
+  , _agDomain      :: [v]
+  , _agId          :: i
+  , _agView        :: Map i v
+  , _agAbove       :: [i]
+  , _agBelow       :: [i]
+  , _agConstraints :: [Constraint i v]
+  , _agNoGoods     :: [NoGood i v]
+  }
+makeLenses ''AgentState
