3cc6b25d99fec88cfa8bc24aff7a9e77b6415d09
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2013-11-24 05:14:35 +0400

    added Control.Agent.Free.Interfaces

---
+++ src/Control/Agent/Free/Interfaces.hs
@@ -0,0 +1,18 @@
+---------------------------------------------------------------------------
+-- |
+-- Module      :  Control.Agent.Free.Interfaces
+-- Copyright   :  (c) Nickolay Kudasov 2013
+-- License     :  BSD-style (see the file LICENSE)
+-- 
+-- Maintainer  :  nickolay.kudasov@gmail.com
+-- Stability   :  experimental
+-- Portability :  ghc
+--
+-- Common interfaces.
+---------------------------------------------------------------------------
+module Control.Agent.Free.Interfaces (
+    module Control.Agent.Free.Interfaces.SendRecv
+) where
+
+import Control.Agent.Free.Interfaces.SendRecv
+
