9bae15dad5a349f9bdc6ac7965da884ac5c94c91
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2013-11-29 20:09:05 +0400

    unfinished termination by snapshots

--- src/Control/Agent/Free/Algorithms/Termination.hs
+++ src/Control/Agent/Free/Algorithms/Termination.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 ---------------------------------------------------------------------------
 -- |
 -- Module      :  Control.Agent.Free.Algorithms.Termination
@@ -14,26 +15,38 @@
 module Control.Agent.Free.Algorithms.Termination (
 ) where
 
+import Control.Arrow
 import Control.Agent.Free
 import Control.Agent.Free.Interfaces.SendRecv
 import Control.Monad.State
+import Control.Monad.Trans.Free
 
 data MessageSnapshot msg
   = MsgSnapshotRequest
-  | MsgSnapshotResponse Snapshot
   | MsgSnapshotMessage msg
+  deriving (Show)
 
-addTerminationBySnapshots :: (Ord i) => Agent (SendRecv i msg) t m a -> Agent (SendRecv i (Int, MessageSnapshot msg)) t m a
-addTerminationBySnapshots = flip evalStateT (0, Nothing) $ transform transF
+addTerminationBySnapshots :: (Ord i, Monad (t m)) => i -> [i] -> Agent (SendRecv i msg) t m a -> Agent (SendRecv i (Int, MessageSnapshot msg)) t m a
+addTerminationBySnapshots myId neighbors = flip evalStateT (0, myId) . transform transF . hoistFreeT lift
   where
     transF (Send i msg next) = do
       time <- gets fst
       send i (time, MsgSnapshotMessage msg)
       next
     transF (Recv next) = do
-      modify (first succ)     -- increment local clock (FIXME: use max with my ID)
-      (i, (n, msg)) <- recv
-      modify (max (n, i))     -- "synchronize" clock
-      -- TODO: ...
-
+      modify (succ *** const myId)
+      x <- gets fst
+      forM_ neighbors $ \i -> send i (x, MsgSnapshotRequest)
+      (i, (t, msg)) <- recv
+      case msg of
+        MsgSnapshotMessage m -> do
+          modify . first $ const (t + 1)
+          next i m
+        MsgSnapshotRequest -> do
+          s <- get
+          if (t, i) > s then do
+            put (t, i)
+            next undefined undefined  -- FIXME
+          else
+            next undefined undefined  -- FIXME: do nothing (repeat recv)
 
